5bd95e48ef7181388a0733dcf4df404d12309b1d
faouaz abdallah <faouaza@yahoo.com>
2023-11-27 22:30:24 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from "./pages/About/About";
 import Footer from "./Components/Footer/Footer";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import FicheLogement from "./pages/FicheLogement/FicheLogement";
-import { Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -19,10 +18,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/Apropos" element={<About />} />
             <Route path="/logement/:id" element={<FicheLogement />} />
-            {/* <Route
-              path="https://githubfaouaz.github.io/Kasa/"
-              element={<Home />}
-            /> */}
             <Route path="*" element={<ErrorPage />} />
           </Routes>
         </Router>
